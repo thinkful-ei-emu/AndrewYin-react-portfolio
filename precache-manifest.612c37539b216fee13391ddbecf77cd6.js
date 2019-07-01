fac9b17971b6c42c52c2238b21e6f234019295fb
Andrew <andrewyin1994@gmail.com>
2019-06-30 18:23:29 -0700

    Updates

---
+++ precache-manifest.612c37539b216fee13391ddbecf77cd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5023e791042c3269270bee28cfb4265",
+    "url": "/AndrewYin-react-portfolio/index.html"
+  },
+  {
+    "revision": "29f6af744b5d24f1737c",
+    "url": "/AndrewYin-react-portfolio/static/css/main.22bfb662.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/AndrewYin-react-portfolio/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "29f6af744b5d24f1737c",
+    "url": "/AndrewYin-react-portfolio/static/js/main.ef8c6263.chunk.js"
+  },
+  {
+    "revision": "a0b035784c284192d55c",
+    "url": "/AndrewYin-react-portfolio/static/js/runtime~main.6b5de333.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7676307bcf2d6c3bea9b350140678ca7.js"
+  "/AndrewYin-react-portfolio/precache-manifest.612c37539b216fee13391ddbecf77cd6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AndrewYin-react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
